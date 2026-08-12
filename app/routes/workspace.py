72ad695a5c89cad9ab36739f9c5ab58d6225f69d
Etsub <sarinamehari@gmail.com>
2026-08-12 01:49:37 -0700

    moving the task route to its own file

--- app/routes/workspace.py
+++ app/routes/workspace.py
@@ -23,19 +23,3 @@ def get_workspace(workspace_id: int):
 def update_workspace(workspace_id: int, workspace: Workspace):
     return {"message": f"Workspace with ID {workspace_id} updated successfully."}
 
-@router.post("/workspace/{workspace_id}/task")
-def add_item_to_workspace(workspace_id: int, task: dict[str, str]):
-    return {"message": f"Task added to workspace with ID {workspace_id}."}
-
-@router.delete("/workspace/{workspace_id}/task/{task_id}")
-def remove_item_from_workspace(workspace_id: int, task_id: int):
-    return {"message": f"Task with ID {task_id} removed from workspace with ID {workspace_id}."}
-
-@router.get("/workspace/{workspace_id}/task/{task_id}")
-def get_item_from_workspace(workspace_id: int, task_id: int):
-    return {"message": f"Details of task with ID {task_id} in workspace with ID {workspace_id}."}
-
-
-
-
-
